a2dc38d783508777ebd2dbba67cfc8d1e62a19df
lewisky <lewisky28mawira@gmail.com>
2023-04-28 12:12:01 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "./Container";
-
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <div class="container">
+      <h1>{props.header}</h1>
+      <p>{props.description}</p>
+    </div>
+
+    
+  );
+}
+
+
+
+
+export default Header;
